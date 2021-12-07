f0f748ba1fe9b2ea59df0aeaac4fac1e7dc24619
wafa-meddeb <95235661+wafa-meddeb@users.noreply.github.com>
2021-12-08 00:32:52 +0100

    Delete snake_game.py

--- snake_game.py
+++
@@ -1,9 +0,0 @@
-from pynput.keyboard import Key, Controller
-import time
-import pynput.keyboard
-keyboard = Controller()
-time.sleep(10)
-keyboard.press(Key.right)
-time.sleep(5)
-keyboard.release(Key.right)
-
